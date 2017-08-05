f579971174fb5f4c7ca95294c5c3076129e7726f
John Wells <madmodmail@gmail.com>
2017-08-04 20:39:37 -0600

    Add error logging to test.js.

--- cli/test.js
+++ cli/test.js
@@ -1,25 +1,22 @@
 var nodecastor = require('nodecastor'),
-	util = require('util');
+  util = require('util');
 nodecastor.scan()
   .on('online', function(d) {
-    console.log("New device: ", d.id);
-    d.on('connect', function() {
-    d.status(function(err, s) {
-      if (!err) {
+    console.log("New device: ", d.id, d.friendlyName);
+    d.on('connect', function(c) {
+      d.status(function(err, s) {
+        if (err) return console.error(err);
         console.log('Chromecast status', util.inspect(s));
         d.application('84912283', function(err, a) {
-          if (!err) {
-            console.log('Application', util.inspect(a));
-            a.run('urn:x-cast:madmod.dashcast', function(err, s) {
-              if (!err) {
-                s.send({url: "http://codepen.io/fleeting/full/xklfq/"});
-              }
-            });
-          }
+          if (err) return console.error(err);
+          console.log('Application', util.inspect(a));
+          a.run('urn:x-cast:madmod.dashcast', function(err, s) {
+            if (err) return console.error(err);
+            s.send({ url: "http://codepen.io/fleeting/full/xklfq/" });
+          });
+        });
       });
-      }
     });
-});
   })
   .on('offline', function(d) {
     console.log('Removed device', util.inspect(d));
